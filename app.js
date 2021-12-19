9e2a22e25c97162d3632a212aa97e6d3d971510e
Sayed <sayedhossainsifat300@gmail.com>
2021-12-19 23:55:42 +0600

    almost done

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+// login button event handler
 const login = document.getElementById("login");
 login.addEventListener("click" , function () {
     const loginArea = document.getElementById("login_area");
@@ -5,3 +7,32 @@ login.addEventListener("click" , function () {
     const transactionArea  = document.getElementById("transaction_area");
     transactionArea.style.display = "block";
 })
+// deposit button event handler
+const depositBtn = document.getElementById("addDeposit");
+depositBtn.addEventListener("click" , function () {
+    const depositNumber = getInputNumber("depositAmount");
+
+    updateSpanText("currentDeposit",depositNumber);
+    updateSpanText ("currentBalance",depositNumber)
+
+    document.querySelector("#depositAmount").value = "";
+});
+// withdraw button event handler 
+const widthdrawBtn = document.getElementById("addWithdraw");
+widthdrawBtn.addEventListener("click" , function () {
+   const withdrawNumber = getInputNumber("withdrawAmount");
+   console.log(withdrawNumber)
+})
+
+function getInputNumber (id) {
+    const amount = document.getElementById(id).value ;
+    const amountNumber = Number(amount);
+    return amountNumber
+}
+
+function updateSpanText (id ,depositNumber) {
+    const current = document.getElementById(id).innerText;
+    const currentNumber = Number(current);
+    const totalAmount = depositNumber + currentNumber ;
+    document.getElementById(id).innerText = totalAmount ;
+}
